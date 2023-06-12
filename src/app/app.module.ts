7b84d45bfc270f8d90169738f0f0d1508822ff83
malubecker <malubecker.adv@gmail.com>
2023-06-12 08:06:41 -0300

    atividade-07

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroComponent,
+    ConsultaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
